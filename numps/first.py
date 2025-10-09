878142a5d386313c53d0c58e2b0a2608c5fb2c00
Aynekulu <Aynekulu4341@gmail.com>
2025-10-09 18:09:45 +0300

    to test the numpy functin

---
+++ numps/first.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Creating a 2D array
+arr = np.array([[1, 2], [3, 4]])
+
+# Accessing the shape and data type
+print(arr.shape)  # Output: (2, 2)
+print(arr.dtype)  # Output: int64
